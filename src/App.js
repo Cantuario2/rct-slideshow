947a44c015557b3716f747e3fbd0059333ba3e5b
cantuario2 <cantuario2@gmail.com>
2022-06-13 18:59:52 -0300

    20220613_housekeeping

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SwiperCore, { Autoplay, EffectCoverflow, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "./App.css";
 
-function App() {
+SwiperCore.use([EffectCoverflow, Autoplay, Pagination]);
+
+const slide_img = [
+  "https://swiperjs.com/demos/images/nature-1.jpg",
+  "https://swiperjs.com/demos/images/nature-2.jpg",
+  "https://swiperjs.com/demos/images/nature-3.jpg",
+  "https://swiperjs.com/demos/images/nature-4.jpg",
+  "https://swiperjs.com/demos/images/nature-5.jpg",
+  "https://swiperjs.com/demos/images/nature-6.jpg",
+  "https://swiperjs.com/demos/images/nature-7.jpg",
+  "https://swiperjs.com/demos/images/nature-8.jpg",
+  "https://swiperjs.com/demos/images/nature-9.jpg",
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-swiper">
+      <Swiper
+        effect={"coverflow"}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        pagination={true}
+        className="mySwiper"
+      >
+        {slide_img.map((img, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={img} alt="" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
-}
+};
 
 export default App;
